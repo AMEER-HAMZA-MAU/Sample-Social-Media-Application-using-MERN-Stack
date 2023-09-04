b0d43bfb69b982de5f167866b1d02c1fabf55a01
AMEER HAMZA <114941881+AMEER-HAMZA-MAU@users.noreply.github.com>
2023-09-04 17:07:36 +0530

    Update deployment-script.sh

--- deployment-script.sh
+++ deployment-script.sh
@@ -7,10 +7,8 @@
 ########################################################
 
 sudo apt-get update && apt-get upgrade
-sudo systemctl enable docker
-sudo systemctl start docker
 echo "build the docker image"
-sudo docker build -t mernstack:latest
+sudo docker build . -t mernstack:latest 
 echo "Deploying the updated container"
 sudo docker run -p 3000:3000 mernstack:latest
 
