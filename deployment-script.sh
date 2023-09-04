76e309308e6328760e6689b7da0edd2ac281926d
AMEER HAMZA <114941881+AMEER-HAMZA-MAU@users.noreply.github.com>
2023-09-04 17:33:33 +0530

    Update deployment-script.sh

--- deployment-script.sh
+++ deployment-script.sh
@@ -6,4 +6,4 @@ sudo apt-get update
 echo "build the docker image"
 sudo docker build . -t mernstack:latest >> /home/hamza/Sample-Social-Media-Application-using-MERN-Stack/Dockerfile
 echo "Deploying the updated container"
-sudo docker run -p 3000:3000 mernstack:latest
+sudo docker container run -p 3000:3000 mernstack:latest
