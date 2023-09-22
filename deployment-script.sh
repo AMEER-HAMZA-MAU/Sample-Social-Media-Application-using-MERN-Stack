e7ba1381c959faaa384ecf7ba0d4b466e22e7be8
AMEER HAMZA <114941881+AMEER-HAMZA-MAU@users.noreply.github.com>
2023-09-22 21:42:42 +0530

    Update deployment-script.sh

--- deployment-script.sh
+++ deployment-script.sh
@@ -3,9 +3,10 @@
 ## Shell Script to Build Docker Image by AMEER HAMZA
 ########################################################
 sudo apt-get update
+sudo apt-get upgrade
+sudo apt-get install docker.io -y
 git clone https://github.com/AMEER-HAMZA-MAU/Sample-Social-Media-Application-using-MERN-Stack.git
 cd /home/hamza/Sample-Social-Media-Application-using-MERN-Stack/
-echo "build the docker image"
-sudo docker build . -t mernstack:latest >> /home/hamza/Sample-Social-Media-Application-using-MERN-Stack/Dockerfile
-echo "Deploying the updated container"
-sudo docker container run -p 3000:3000 mernstack:latest
+echo "Run Docker compose file"
+docker-compose up
+
